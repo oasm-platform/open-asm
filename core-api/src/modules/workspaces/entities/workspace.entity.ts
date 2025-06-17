84e2eb7201b8e934f6bdba99b0b573e31b9a02ef
TuanThanh2kk4 <tuanthanh2kk4@gmail.com>
2025-06-17 09:09:23 +0700

    feat: add ApiProperty in entities

--- core-api/src/modules/workspaces/entities/workspace.entity.ts
+++ core-api/src/modules/workspaces/entities/workspace.entity.ts
@@ -8,24 +8,36 @@ import {
 } from 'typeorm';
 import { WorkspaceMembers } from './workspace-members.entity';
 import { User } from 'src/modules/auth/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
-@Entity()
+@Entity('workspace')
 export class Workspace extends BaseEntity {
+  @ApiProperty({
+    example: 'My Workspace',
+    description: 'The name of the workspace',
+  })
   @Column('text')
   name: string;
 
+  @ApiProperty({
+    example: 'This is my workspace',
+    description: 'The description of the workspace',
+  })
   @Column('text', { nullable: true })
   description?: string;
 
+  @ApiProperty({ description: 'The owner of the workspace' })
   @ManyToOne(() => User, (user) => user.workspaces)
   ownerId: User;
 
+  @ApiProperty({ description: 'The members of the workspace' })
   @OneToMany(
     () => WorkspaceMembers,
     (workspaceMembers) => workspaceMembers.workspace,
   )
   workspaceMembers: WorkspaceMembers[];
 
+  @ApiProperty({ description: 'The date the workspace was softdeleted' })
   @DeleteDateColumn()
   deletedAt?: Date;
 }
--- core-api/src/modules/workspaces/workspaces.service.ts
+++ core-api/src/modules/workspaces/workspaces.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Workspace } from './entities/workspace.entity';
+import { Repository } from 'typeorm';
+import { WorkspaceMembers } from './entities/workspace-members.entity';
 
 @Injectable()
-export class WorkspacesService {}
+export class WorkspacesService {
+  constructor(
+    @InjectRepository(Workspace)
+    private readonly workspaceRepository: Repository<Workspace>,
+    @InjectRepository(WorkspaceMembers)
+    private readonly workspaceMembersRepository: Repository<WorkspaceMembers>,
+  ) {}
+}
