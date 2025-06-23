2fca7c86e43c12f1a26e78bdc178b5ec7cad9ab5
TuanThanh2kk4 <tuanthanh2kk4@gmail.com>
2025-06-23 23:22:10 +0700

    feat: get all assets in target

--- core-api/src/modules/assets/assets.controller.ts
+++ core-api/src/modules/assets/assets.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AssetsService } from './assets.service';
+import { IdQueryParamDto } from 'src/common/dtos/id-query-param.dto';
+import { Doc } from 'src/common/doc/doc.decorator';
+import { Asset } from './entities/assets.entity';
+import {
+  GetManyBaseQueryParams,
+  GetManyResponseDto,
+} from 'src/common/dtos/get-many-base.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Assets')
 @Controller('assets')
 export class AssetsController {
   constructor(private readonly assetsService: AssetsService) {}
+
+  @Doc({
+    summary: 'Get assets in target',
+    description: 'Retrieves a list of assets associated with the given target.',
+    response: {
+      serialization: GetManyResponseDto<Asset>,
+    },
+  })
+  @Get('/target/:id')
+  getAllAssetsInTarget(
+    @Param() { id }: IdQueryParamDto,
+    @Query() query: GetManyBaseQueryParams,
+  ) {
+    return this.assetsService.getAllAssetsInTarget(id, query);
+  }
 }
