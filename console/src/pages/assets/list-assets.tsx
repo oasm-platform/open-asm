aa72c078d2ed9e012d2e2644b2aa8638a1527074
mizhm <minhnt141204@gmail.com>
2025-08-26 18:44:49 +0700

    refactor(console, asset): refactor tab to make api call only when selected

--- console/src/pages/assets/list-assets.tsx
+++ console/src/pages/assets/list-assets.tsx
@@ -1,57 +1,76 @@
-import { Tabs } from "@/components/ui/tabs";
-import AssetTabContent from "./components/asset-tab";
-import FilterForm from "./components/filter-form";
-import IpAssetsTab from "./components/ip-assets-tab";
-import TriggerList from "./components/tab-trigger-list";
-import PortAssetsTab from "./components/port-assets-tab";
-import TechnologyAssetsTab from "./components/technology-assets-tab";
+import { Tabs } from '@/components/ui/tabs';
+import AssetTabContent from './components/asset-tab';
+import FilterForm from './components/filter-form';
+import IpAssetsTab from './components/ip-assets-tab';
+import TriggerList from './components/tab-trigger-list';
+import PortAssetsTab from './components/port-assets-tab';
+import TechnologyAssetsTab from './components/technology-assets-tab';
+import { useMemo, useState } from 'react';
 
 interface ListAssetsProps {
   targetId?: string;
   refetchInterval?: number;
 }
 
-const tabList = [
-  {
-    value: "asset",
-    text: "All Services",
-  },
-  {
-    value: "tech",
-    text: "Technologies",
-  },
-  {
-    value: "ip",
-    text: "IP Adresses",
-  },
-  {
-    value: "port",
-    text: "Ports",
-  },
-];
-
 export function ListAssets({ targetId, refetchInterval }: ListAssetsProps) {
   //TODO: rework on refetchInterval because not necessary after complete discovering
+  //FIX: fix api call only when selecting tab
+
+  const tabList = useMemo(
+    () => [
+      {
+        value: 'asset',
+        text: 'All Services',
+        tab: (
+          <AssetTabContent
+            refetchInterval={refetchInterval}
+            targetId={targetId}
+          />
+        ),
+      },
+      {
+        value: 'tech',
+        text: 'Technologies',
+        tab: (
+          <TechnologyAssetsTab
+            refetchInterval={refetchInterval}
+            targetId={targetId}
+          />
+        ),
+      },
+      {
+        value: 'ip',
+        text: 'IP Adresses',
+        tab: (
+          <IpAssetsTab refetchInterval={refetchInterval} targetId={targetId} />
+        ),
+      },
+      {
+        value: 'port',
+        text: 'Ports',
+        tab: (
+          <PortAssetsTab
+            refetchInterval={refetchInterval}
+            targetId={targetId}
+          />
+        ),
+      },
+    ],
+    [refetchInterval, targetId],
+  );
+
+  const [selectedTab, setSelectedTab] = useState('asset');
+
   return (
     <div className="w-full">
       <FilterForm />
-      <Tabs defaultValue="asset" className="gap-0">
+      <Tabs
+        value={selectedTab}
+        onValueChange={setSelectedTab}
+        className="gap-0"
+      >
         <TriggerList tabTriggerList={tabList} />
-        <div>
-          <AssetTabContent
-            targetId={targetId}
-            refetchInterval={refetchInterval}
-          />
-          <IpAssetsTab targetId={targetId} refetchInterval={refetchInterval} />
-          <PortAssetsTab
-            targetId={targetId}
-            refetchInterval={refetchInterval}
-          />
-          <TechnologyAssetsTab
-            targetId={targetId}
-            refetchInterval={refetchInterval}
-          />
-        </div>
+        {tabList.find((a) => a.value == selectedTab)?.tab}
       </Tabs>
     </div>
   );
