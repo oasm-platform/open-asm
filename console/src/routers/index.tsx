22700fab1c3add241c9d9cf73125bed2740def5c
mizhm <minhnt141204@gmail.com>
2025-09-06 23:07:16 +0700

    fix(console, router): remove duplicate import after merge main

--- console/src/routers/index.tsx
+++ console/src/routers/index.tsx
@@ -2,7 +2,11 @@ import Layout from '@/components/common/layout/layout';
 import Assets from '@/pages/assets/assets';
 import DetailAsset from '@/pages/assets/detail-asset';
 import Dashboard from '@/pages/dashboard/dashboard';
+import Editor from '@/pages/editor/editor';
 import Login from '@/pages/login/login';
+import CreateProviderPage from '@/pages/providers/create-provider';
+import DetailProvider from '@/pages/providers/detail-provider';
+import EditProviderPage from '@/pages/providers/edit-provider';
 import ProvidersPage from '@/pages/providers/providers';
 import Register from '@/pages/register/register';
 import Search from '@/pages/search/search';
@@ -18,30 +22,6 @@ import GuestRoute from './GuestRoute';
 import NotFound from './NotFound';
 import ProtectedRoute from './ProtectedRoute';
 import RegisterRoute from './RegisterRoute';
-import Editor from '@/pages/editor/editor';
-import Layout from "@/components/common/layout/layout";
-import Assets from "@/pages/assets/assets";
-import DetailAsset from "@/pages/assets/detail-asset";
-import Dashboard from "@/pages/dashboard/dashboard";
-import Login from "@/pages/login/login";
-import ProvidersPage from "@/pages/providers/providers";
-import DetailProvider from "@/pages/providers/detail-provider";
-import CreateProviderPage from "@/pages/providers/create-provider";
-import EditProviderPage from "@/pages/providers/edit-provider";
-import Register from "@/pages/register/register";
-import Search from "@/pages/search/search";
-import Settings from "@/pages/settings/settings";
-import DetailTarget from "@/pages/targets/detail-target";
-import Targets from "@/pages/targets/targets";
-import ToolDetail from "@/pages/tools/components/tool-detail";
-import Tools from "@/pages/tools/tools";
-import Vulnerabilities from "@/pages/vulnerabilities/vulnerabilities";
-import Workers from "@/pages/workers/workers";
-import { createBrowserRouter } from "react-router-dom";
-import GuestRoute from "./GuestRoute";
-import NotFound from "./NotFound";
-import ProtectedRoute from "./ProtectedRoute";
-import RegisterRoute from "./RegisterRoute";
 
 export const router = createBrowserRouter([
   {
@@ -140,22 +120,22 @@ export const router = createBrowserRouter([
             ],
           },
           {
-            path: "providers",
+            path: 'providers',
             children: [
               {
-                path: "",
+                path: '',
                 element: <ProvidersPage />,
               },
               {
-                path: "create",
+                path: 'create',
                 element: <CreateProviderPage />,
               },
               {
-                path: ":id",
+                path: ':id',
                 element: <DetailProvider />,
               },
               {
-                path: ":id/edit",
+                path: ':id/edit',
                 element: <EditProviderPage />,
               },
             ],
