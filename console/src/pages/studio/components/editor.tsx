b0a1b6960036c99b59274b8630c8186a671bd79b
mizhm <minhnt141204@gmail.com>
2025-09-13 09:49:03 +0700

    feat(studio): add formatting before saving template

--- console/src/pages/studio/components/editor.tsx
+++ console/src/pages/studio/components/editor.tsx
@@ -8,11 +8,13 @@ import { yaml } from '@codemirror/lang-yaml';
 import { tokyoNight } from '@uiw/codemirror-theme-tokyo-night';
 import CodeMirror from '@uiw/react-codemirror';
 import { useAtom, useSetAtom } from 'jotai';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useMemo, useRef } from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
 import { toast } from 'sonner';
 import { activeTemplateAtom, activeTemplateIdAtom } from '../atoms';
 import { ScanComponent } from './scan-component';
+import * as prettier from 'prettier/standalone';
+import * as prettierYaml from 'prettier/plugins/yaml';
 
 export default function Editor() {
   const [activeTemplate, setActiveTemplate] = useAtom(activeTemplateAtom);
@@ -25,52 +27,99 @@ export default function Editor() {
     activeTemplate?.id || '',
   );
 
-  let bucket = '';
-  let path = '';
-  if (data && data.path) {
-    bucket = data.path.split('/')[0];
-    path = data.path.split('/')[1];
-  }
+  const [bucket, path] = useMemo(() => data?.path?.split('/') || [], [data]);
 
-  const { data: fileData } = useStorageControllerGetFile(bucket, path);
+  const { data: fileData, refetch } = useStorageControllerGetFile(bucket, path);
   const contentSaved = useRef(activeTemplate?.content);
 
   useEffect(() => {
     const readFile = async () => {
       if (fileData instanceof Blob) {
-        contentSaved.current = await fileData.text();
+        const content = await fileData.text();
+        contentSaved.current = content;
+        setActiveTemplate({ content });
       }
     };
     readFile();
-  }, [fileData]);
+  }, [fileData, setActiveTemplate]);
+
+  const formatYAML = useCallback(async () => {
+    if (!activeTemplate?.content) {
+      toast.error('No content to format', { closeButton: true });
+      return false;
+    }
+    const toastId = toast.loading('Formating template...', {
+      closeButton: true,
+    });
+    try {
+      const formatted = await prettier.format(activeTemplate.content, {
+        parser: 'yaml',
+        plugins: [prettierYaml],
+      });
+
+      setActiveTemplate({ ...activeTemplate, content: formatted });
+      toast.success('Template formatted successfully', {
+        closeButton: true,
+        id: toastId,
+      });
+    } catch {
+      toast.error(
+        'Error happened, please check the template before formatting',
+        {
+          closeButton: true,
+          id: toastId,
+        },
+      );
+      return false;
+    }
+    return true;
+  }, [activeTemplate, setActiveTemplate]);
+
+  useHotkeys(
+    'ctrl+shift+f',
+    (e) => {
+      e.preventDefault();
+      formatYAML();
+    },
+    { enableOnContentEditable: true },
+  );
 
   useHotkeys(
     'ctrl+s',
     async (e) => {
       e.preventDefault();
+      const isFormatted = await formatYAML();
+      if (!isFormatted) return;
       if (activeTemplate && contentSaved.current !== activeTemplate.content) {
         if (!data) {
-          const template = await createTemplate({
-            data: {
-              fileName: activeTemplate.filename,
+          await createTemplate(
+            {
+              data: {
+                fileName: activeTemplate.filename,
+              },
             },
-          });
-          uploadTemplate({
-            data: {
-              fileContent: activeTemplate.content,
-              templateId: template.id,
+            {
+              onSuccess: (data) => {
+                setActiveTemplate({ id: data.id });
+                setActiveTemplateId(data.id);
+              },
             },
-          });
-          setActiveTemplate({ id: template.id });
-          setActiveTemplateId(template.id);
-        } else {
-          uploadTemplate({
-            data: {
-              fileContent: activeTemplate.content,
-              templateId: data.id,
-            },
-          });
+          );
         }
+        uploadTemplate(
+          {
+            data: {
+              fileContent: activeTemplate.content,
+              templateId: activeTemplate.id,
+            },
+          },
+          {
+            onSuccess: () => {
+              refetch();
+            },
+          },
+        );
+        toast.success('Template is saved successfully');
       } else {
         toast.warning('You have not made any changes', {
           closeButton: true,
