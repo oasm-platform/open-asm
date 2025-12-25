6b1be95c20a3b86a536445e8e482bf1657dcba8d
l1ttps <l1ttps443@gmail.com>
2025-12-25 02:53:30 +0000

    refactor(technology): optimize initialization and caching logic

--- core-api/src/modules/assets/assets.module.ts
+++ core-api/src/modules/assets/assets.module.ts
@@ -2,14 +2,13 @@ import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Job } from '../jobs-registry/entities/job.entity';
 import { Target } from '../targets/entities/target.entity';
-import { TechnologyForwarderService } from '../technology/technology-forwarder.service';
 import { TechnologyModule } from '../technology/technology.module';
 import { WorkspacesModule } from '../workspaces/workspaces.module';
 import { AssetsController } from './assets.controller';
 import { AssetsService } from './assets.service';
+import { AssetService } from './entities/asset-services.entity';
 import { Asset } from './entities/assets.entity';
 import { HttpResponse } from './entities/http-response.entity';
-import { AssetService } from './entities/asset-services.entity';
 
 @Global()
 @Module({
@@ -19,7 +18,7 @@ import { AssetService } from './entities/asset-services.entity';
     WorkspacesModule,
   ],
   controllers: [AssetsController],
-  providers: [AssetsService, TechnologyForwarderService],
+  providers: [AssetsService],
   exports: [AssetsService],
 })
 export class AssetsModule {}
