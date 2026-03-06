9db08241abdf319a336e2b885ae9b3b5eff78c50
l1ttps <l1ttps443@gmail.com>
2026-03-06 13:01:15 +0000

    feat(console): add all workspaces navigation and improve 404 page UI

--- console/src/routers/ProtectedRoute.tsx
+++ console/src/routers/ProtectedRoute.tsx
@@ -1,24 +1,15 @@
-import StudioLayout from '@/components/common/layout/studio-layout';
 import ProtectedLayout from '@/components/common/layout/protect-layout';
 import { authClient } from '@/utils/authClient';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 const { useSession } = authClient;
 
 const ProtectedRoute = () => {
-  const { data, isPending: isLoadingSession } = useSession();
+  const { data, isPending } = useSession();
   const location = useLocation();
   const currentPath = location.pathname;
 
-  if (!data && !isLoadingSession) {
+  if (!data && !isPending) {
     return <Navigate to={`/login?redirect=${currentPath}`} />;
-  }
-
-  if (currentPath.startsWith('/studio')) {
-    return (
-      <StudioLayout>
-        <Outlet />
-      </StudioLayout>
-    );
   }
 
   return (
