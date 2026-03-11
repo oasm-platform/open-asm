5c9926971ce0bffe46ba84927a3e198fd43fbe93
l1ttps <l1ttps443@gmail.com>
2026-03-11 17:53:46 +0000

    chore(db): add typeorm migration scripts and configure datasource

--- core-api/src/database/database-config.ts
+++ core-api/src/database/database-config.ts
@@ -1,4 +1,7 @@
-import { Pool } from 'pg';
+import 'dotenv/config';
+import type { DataSourceOptions } from 'typeorm';
+import { DataSource } from 'typeorm';
+
 const {
   POSTGRES_HOST,
   POSTGRES_USERNAME,
@@ -13,10 +16,19 @@ export const databaseConnectionConfig = {
   user: POSTGRES_USERNAME,
   username: POSTGRES_USERNAME,
   password: POSTGRES_PASSWORD,
-  port: parseInt(POSTGRES_PORT || '5432', 5432),
+  port: parseInt(POSTGRES_PORT || '5432', 10),
   database: POSTGRES_DB,
-  ssl: Boolean(POSTGRES_SSL === 'true'),
+  ssl: POSTGRES_SSL === 'true',
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-export const database = new Pool(databaseConnectionConfig);
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  ...databaseConnectionConfig,
+  synchronize: false,
+  entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  migrationsRun: true,
+  migrationsTableName: 'migrations',
+};
+
+export const AppDataSource = new DataSource(dataSourceOptions);
--- core-api/src/database/database.module.ts
+++ core-api/src/database/database.module.ts
@@ -1,19 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { databaseConnectionConfig } from './database-config';
+import { dataSourceOptions } from './database-config';
 import { DatabaseService } from './database.service';
 
 @Global()
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
-      ...databaseConnectionConfig,
-      synchronize: true,
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
   providers: [DatabaseService],
   exports: [DatabaseService],
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
