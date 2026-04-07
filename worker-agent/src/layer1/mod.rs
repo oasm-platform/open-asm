798c54176a81bffd83e7744fd33992e517cde2b2
mizhm <minhnt141204@gmail.com>
2026-04-07 21:49:08 +0700

    chore(gitignore): update gitignore

--- worker-agent/src/layer1/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod grpc_client;
-pub mod auth;
-pub mod job_poller;
-pub mod heartbeat;
-pub mod manifest;
--- worker-agent/src/layer2/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod tool_manager;
-pub mod job_processor;
-
-pub use tool_manager::ToolManager;
-pub use job_processor::JobProcessor;
