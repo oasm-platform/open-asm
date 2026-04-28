a3e508b7be62b5099cf6715049d3daaeaa49e3ba
l1ttps <l1ttps443@gmail.com>
2026-04-28 03:15:40 +0000

    feat(worker): add network interface detection logic

--- worker/cmd/oasm-worker/main.go
+++ worker/cmd/oasm-worker/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "oasm-worker/internal/cli"
+import (
+	"fmt"
+	"log"
+	"oasm-worker/internal/cli"
+	"oasm-worker/internal/worker"
+)
 
 func main() {
+	// Test network info
+	info, err := worker.GetNetworkInfo()
+	if err != nil {
+		log.Printf("Error getting network info: %v", err)
+	} else {
+		fmt.Printf("Selected Interface: %s\n", info.Interface)
+		fmt.Printf("IP: %s\n", info.IP)
+		fmt.Printf("CIDR: %s\n", info.CIDR)
+		fmt.Printf("Gateway IP: %s\n", info.GatewayIP)
+		fmt.Printf("Gateway MAC: %s\n", info.GatewayMAC)
+	}
+
 	cli.Execute()
 }
