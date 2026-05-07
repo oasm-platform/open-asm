504521713368e3030c1b6cd51fff0321684e4ece
l1ttps <l1ttps443@gmail.com>
2026-05-07 12:15:28 +0000

    chore(console): cleanup css variables and adjust dashboard layout

--- console/src/pages/dashboard/dashboard.tsx
+++ console/src/pages/dashboard/dashboard.tsx
@@ -2,13 +2,13 @@ import Page from '@/components/common/page';
 import { useWorkspaceSelector } from '@/hooks/useWorkspaceSelector';
 import CreateWorkspace from '../workspaces/create-workspace';
 import AssetLocationsMap from './components/asset-locations-map';
+import { AssetTrends } from './components/asset-trends';
 import IssuesTimeline from './components/issues-timeline';
 import Statistic from './components/statistic';
 import TlsExpirationTable from './components/tls-expiration-table';
 import TopAssetsVulnerabilitiesChart from './components/top-assets-vulnerabilities-chart';
 import TopTagsAssets from './components/top-tags-assets';
 import VulnerabilityStatistic from './components/vulnerabilities-statistic';
-import { AssetTrends } from './components/asset-trends';
 
 export default function Dashboard() {
   const { workspaces, isLoading } = useWorkspaceSelector();
@@ -39,7 +39,7 @@ export default function Dashboard() {
               <TopTagsAssets />
             </div>
           </div>
-          <div className="col-span-1 2xl:col-span-4 space-y-4 min-h-96 order-last">
+          <div className="col-span-1 2xl:col-span-3 space-y-4 min-h-96 order-last">
             <AssetLocationsMap />
           </div>
         </div>
