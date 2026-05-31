bf2d917d03eabe5960f54c05024c724a811d01d4
mizhm <minhnt141204@gmail.com>
2026-05-31 17:23:51 +0700

    fix(console): fix e2e github ci

--- console/e2e/playwright.config.ts
+++ console/e2e/playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './',
+  testMatch: '**/*.spec.ts',
   timeout: 30_000,
   retries: 1,
   workers: process.env.CI ? 2 : 4,
