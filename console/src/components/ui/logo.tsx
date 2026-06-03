460871d13e7269f86ee9496fd257f61b2fc4dd37
l1ttps <l1ttps443@gmail.com>
2026-06-03 14:50:08 +0000

    style(ui): use width and height props in Logo component

--- console/src/components/ui/logo.tsx
+++ console/src/components/ui/logo.tsx
@@ -3,13 +3,13 @@ interface LogoProps {
   height?: number;
   logoPath?: string;
 }
-export default function Logo({ width = 16, height = 16, logoPath }: LogoProps) {
+export default function Logo({ width, height, logoPath }: LogoProps) {
   return (
     <>
       {logoPath ? (
-        <img src={logoPath} alt="logo" className="w-13 rounded-md" />
+        <img src={logoPath} alt="logo" width={width} height={height} className="rounded-md" />
       ) : (
-        <img src="/logo.png" alt="logo" className="w-7 h-7 rounded-md" />
+        <img src="/logo.png" alt="logo" width={width} height={height} className="rounded-md" />
       )}
     </>
   );
