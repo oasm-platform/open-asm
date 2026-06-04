8e466d73366da6a0d38a3182f177de4767bdf32d
mizhm <minhnt141204@gmail.com>
2026-06-04 22:54:04 +0700

    chore(console): cleanup junk

--- console/src/components/ui/workspace-switcher.tsx
+++ console/src/components/ui/workspace-switcher.tsx
@@ -66,12 +66,12 @@ export function WorkspaceSwitcher() {
             {workspaces.map((workspace) => (
               <DropdownMenuItem
                 key={workspace.name}
-                onClick={(e) => {
+                onSelect={(e) => {
                   if (isMobile) {
                     e.preventDefault();
                   }
                   handleSelectWorkspace(workspace.id);
-                  toast(`Switched to ${workspace.name}`);
+                  toast('Switched to ' + workspace.name);
                   navigate({ to: '/' });
                 }}
                 className="gap-2 p-2"
@@ -107,64 +107,4 @@ export function WorkspaceSwitcher() {
       </SidebarMenuItem>
     </SidebarMenu>
   );
-  // return (
-  //   <DropdownMenu>
-  //     <DropdownMenuTrigger asChild className="border">
-  //       <SidebarMenuButton
-  //         size="lg"
-  //         className={`${itemHeightClass} data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground`}
-  //       >
-  //         <div className="flex flex-col gap-0.5 leading-none">
-  //           <span className="font-semibold">
-  //             {workspaces.find((ws) => ws.id === selectedWorkspace)?.name ||
-  //               'Select workspace'}
-  //           </span>
-  //         </div>
-  //         <ChevronsUpDown className="ml-auto" />
-  //       </SidebarMenuButton>
-  //     </DropdownMenuTrigger>
-
-  //     <DropdownMenuContent
-  //       align="start"
-  //       className="min-w-(--radix-dropdown-menu-trigger-width) p-2"
-  //     >
-  //       {workspaces.length > 0 ? (
-  //         <>
-  //           {workspaces.map((workspace) => (
-  //             <DropdownMenuItem
-  //               key={workspace.id}
-  //               onSelect={(event) => {
-  //                 // Prevent default behavior on mobile to ensure proper closing
-  //                 if (isMobile) {
-  //                   event.preventDefault();
-  //                 }
-  //                 handleSelectWorkspace(workspace.id);
-  //                 toast(`Switched to ${workspace.name}`);
-  //               }}
-  //               className="px-2 py-1.5 rounded hover:bg-muted flex items-center justify-between"
-  //             >
-  //               {workspace.name}
-  //               {workspace.id === selectedWorkspace && <Check size={16} />}
-  //             </DropdownMenuItem>
-  //           ))}
-  //           <Separator className="my-1" />
-  //         </>
-  //       ) : (
-  //         <div className="text-center text-sm text-muted-foreground px-2 py-4">
-  //           No workspace
-  //         </div>
-  //       )}
-
-  //       <DropdownMenuItem onClick={() => navigate({ to: '/workspaces' })}>
-  //         All workspaces
-  //       </DropdownMenuItem>
-  //       <DropdownMenuItem
-  //         onClick={() => navigate({ to: '/workspaces/create' })}
-  //       >
-  //         <Plus size={16} className="mr-2" />
-  //         Create workspace
-  //       </DropdownMenuItem>
-  //     </DropdownMenuContent>
-  //   </DropdownMenu>
-  // );
 }
